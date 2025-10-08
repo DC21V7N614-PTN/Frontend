6b7a306488f1e5bc64b735dcf2b069c987f37195
DC21V7N614-PTN <nhandc21v7n614@vlvh.ctu.edu.vn>
2025-10-08 23:01:01 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,29 @@
 import { createWebHistory, createRouter } from "vue-router";
-import ContactBook from "../views/ContactBook.vue";
+import ContactBook from "@/views/ContactBook.vue";
 const routes = [{
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
-    path: "/:pathMatch(.*)*",
-    name: "notfound",
-    component: () =>
-        import ("@/views/NotFound.vue"),
-}, ];
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "not-found",
+        component: () =>
+            import ("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () =>
+            import ("@/views/ContactEdit.vue"),
+        props: true,
+    },
+];
+
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
     routes,
 });
+
 export default router;
